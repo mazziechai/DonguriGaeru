df112ef137bae1860f6f46b1a6057b1a0122621c
Mazzie <mazziechai@gmail.com>
2021-09-23 01:01:26 -0700

    Match listing method and player match sorting

--- donguri_gaeru/cogs/info.py
+++ donguri_gaeru/cogs/info.py
@@ -24,6 +24,7 @@ from database import Match, Player
 from discord.ext import commands
 from pytz import UTC
 from sqlalchemy import or_, select
+from sqlalchemy.sql.expression import Select
 from utils import helpers
 
 from donguri_gaeru import Session
@@ -61,28 +62,26 @@ class InfoCog(commands.Cog):
                 await ctx.send(
                     f"__{player.name}__\nMatches played: {len(player.matches)}\n"
                     f"Registered {helpers.time(player.created)}\n"
-                    f"Add `recent` to the end of this command to get {player.name}'s "
-                    f"recent matches or `all` to get all matches."
+                    f"Add `desc` to the end of this command to get {player.name}'s "
+                    "most recent matches or `asc` to get matches from the beginning."
                 )
-            elif type == "recent":
-                stmt = (
-                    select(Match)
-                    .where(
-                        or_(
-                            player.id == Match.playerA_id, player.id == Match.playerB_id
-                        )
-                    )
-                    .order_by(Match.created.desc())
-                    .limit(5)
-                )
-                matches = session.execute(stmt).scalars().all()
-                text = ""
-                for match in matches:
-                    text += helpers.format_match(match)
+                return
 
-                await ctx.send(f"__{player.name}__\nRecent matches:{text}")
-            elif type == "all":
-                await ctx.send("Not implemented yet!")
+            stmt = (
+                select(Match)
+                .where(
+                    or_(player.id == Match.playerA_id, player.id == Match.playerB_id)
+                )
+                .limit(5)
+            )
+
+            if type == "desc":
+                stmt = stmt.order_by(Match.created.desc())
+
+            if type == "asc":
+                stmt = stmt.order_by(Match.created.asc())
+
+            await self.interactive_match_listing(ctx, session, stmt)
 
     @commands.group()
     async def match(self, ctx: commands.Context):
@@ -102,80 +101,88 @@ class InfoCog(commands.Cog):
 
     @match.command()
     async def date(self, ctx: commands.Context, start: str, end: str = None):
-        with Session() as session:
+        try:
+            start_date = datetime.strptime(start, "%Y-%m-%d")
+            start_date = UTC.localize(start_date)
+        except ValueError:
+            await ctx.send("Dates must be in `YYYY-MM-DD` format.")
+            return
+
+        stmt = select(Match).where(Match.created >= start_date)
+
+        if end is not None:
             try:
-                start_date = datetime.strptime(start, "%Y-%m-%d")
-                start_date = UTC.localize(start_date)
+                end_date = datetime.strptime(end, "%Y-%m-%d")
+                end_date = UTC.localize(end_date)
             except ValueError:
-                await ctx.send("Dates must be in YYYY-MM-DD format.")
+                await ctx.send("Dates must be in YYYY-MM-DD format!")
                 return
 
-            stmt = select(Match).where(Match.created >= start_date)
+            if start_date > end_date:
+                await ctx.send("Start date cannot come after the end!")
+                return
+            if start_date == end_date:
+                await ctx.send("Start and end date cannot be the same!")
 
-            if end is not None:
-                try:
-                    end_date = datetime.strptime(end, "%Y-%m-%d")
-                    end_date = UTC.localize(end_date)
-                except ValueError:
-                    await ctx.send("Dates must be in YYYY-MM-DD format!")
-                    return
+            stmt = stmt.where(Match.created <= end_date + timedelta(days=1))
 
-                if start_date > end_date:
-                    await ctx.send("Start date cannot come after the end!")
-                    return
-                if start_date == end_date:
-                    await ctx.send("Start and end date cannot be the same!")
+        else:
+            stmt = stmt.where(Match.created <= start_date + timedelta(days=1))
 
-                stmt = stmt.where(Match.created <= end_date)
+        with Session() as session:
+            await self.interactive_match_listing(ctx, session, stmt)
 
-            else:
-                stmt = stmt.where(Match.created <= start_date + timedelta(days=1))
-
-            iterations = 0
-
-            while True:
-                matches = (
-                    session.execute(stmt.limit(5).offset(iterations * 5))
-                    .scalars()
-                    .fetchmany(5)
+    async def interactive_match_listing(
+        self, ctx: commands.Context, session, stmt: Select
+    ):
+        iterations = 0
+        while True:
+            matches: list[Match] = (
+                session.execute(
+                    stmt.where(Match.active).limit(5).offset(iterations * 5)
                 )
-                if matches == []:
-                    await ctx.send("No matches found!")
-                    break
+                .scalars()
+                .fetchmany(5)
+            )
+            if matches == []:
+                await ctx.send("No matches found!")
+                break
 
-                text = ""
-                for match in matches:
-                    text += helpers.format_match(match)
+            text = ""
+            for match in matches:
+                text += helpers.format_match(match)
 
-                if len(matches) == 5:
-                    await ctx.send(
-                        "Showing 5 matches.\n"
-                        "Type `more` to get more matches. Type `exit` to cancel.\n\n"
-                        + text
+            if len(matches) == 5:
+                await ctx.send(
+                    "Showing 5 matches.\n"
+                    "Type `more` to get more matches or `exit` to cancel.\n\n" + text
+                )
+
+                try:
+                    msg: discord.Message = await self.bot.wait_for(
+                        "message",
+                        check=lambda msg: (
+                            (msg.content == "more" or msg.content == "exit")
+                            and msg.author == ctx.author
+                        ),
+                        timeout=15,
                     )
-
-                    try:
-                        msg: discord.Message = await self.bot.wait_for(
-                            "message",
-                            check=lambda msg: (
-                                (msg.content == "more" or msg.content == "exit")
-                                and msg.author == ctx.author
-                            ),
-                            timeout=15,
-                        )
-
-                        if msg.content == "exit":
-                            await ctx.send("Exiting!")
-                            return
-                    except asyncio.TimeoutError:
+                    # more = back to beginning of loop
+                    if msg.content == "exit":
                         await ctx.send("Exiting!")
                         return
+                except asyncio.TimeoutError:
+                    await ctx.send("Exiting!")
+                    return
 
-                    iterations += 1
+            else:
+                await ctx.send(
+                    f"Showing {len(matches)} "
+                    f"{'match' if len(matches) == 1 else 'matches'}.\n\n" + text
+                )
+                return
 
-                else:
-                    await ctx.send(f"Showing {len(matches)} matches.\n\n" + text)
-                    break
+            iterations += 1
 
 
 def setup(bot: DonguriGaeruBot):
